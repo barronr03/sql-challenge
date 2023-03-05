603fe84667ea0ef60d2f8ec03d3b99dbe3bd8021
barronr03 <roberto_geronimo@yahoo.com>
2023-03-05 14:48:03 -0600

    schemas ready

--- EmployeeSQL/departments.sql
+++ EmployeeSQL/departments.sql
@@ -1,46 +1,63 @@
-SELECT * FROM dept_emp
-
-DROP TABLE dept_emp
-
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-    dept_no	varchar NOT NULL
+CREATE TABLE titles (
+	title_id VARCHAR (10) NOT NULL PRIMARY KEY,
+	title VARCHAR NOT NULL
 );
 
-
-CREATE TABLE dept_manager (
-	dept_no	varchar NOT NULL,
-	emp_no INT NOT NULL
-);
-
-DROP TABLE dept_manager
-
-SELECT * FROM dept_manager
+SELECT * FROM titles
 
 CREATE TABLE employees (
-	emp_no INT NOT NULL,
+	emp_no INT NOT NULL PRIMARY KEY,
 	emp_title_id VARCHAR (10) NOT NULL,
 	birth_date DATE,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	sex VARCHAR NOT NULL,
-	hire_date DATE
+	hire_date DATE,
+	FOREIGN KEY (emp_title_id ) REFERENCES titles(title_id)
 );
 
 SELECT * FROM employees
 
 CREATE TABLE salaries (
+	salary_id SERIAL PRIMARY KEY,
 	emp_no INT NOT NULL,
-	salary INT NOT NULL
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 SELECT * FROM salaries
 
-
-CREATE TABLE titles (
-	title_id VARCHAR (10) NOT NULL,
-	title VARCHAR NOT NULL
+CREATE TABLE departments (
+	dept_no	varchar NOT NULL PRIMARY KEY,
+    dept_name varchar NOT NULL
 );
 
-SELECT * FROM titles
+SELECT * FROM departments
+
+CREATE TABLE dept_emp (
+	id SERIAL PRIMARY KEY,
+	emp_no INT NOT NULL,
+    dept_no	varchar NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_emp (
+	id SERIAL PRIMARY KEY,
+	emp_no INT NOT NULL,
+    dept_no	varchar NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_emp
+
+CREATE TABLE dept_manager (
+	manager_id SERIAL PRIMARY KEY,
+	dept_no	varchar NOT NULL,
+	emp_no INT NOT NULL
+);
+
+
+SELECT * FROM dept_manager
 
